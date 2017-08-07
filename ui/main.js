50c3fffbbf4492acf4b5a35413c51da7f77a96eb
shubham000000 <borghare.sb@gmail.com>
2017-08-06 22:31:11 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ function moveRight(){
     img.style.marginLeft = marginLeft + 'px';
 }
  img.onclick = function(){
-     var interval = setInterval(moveRight, 100);
+     var interval = setInterval(moveRight, 50);
     img.style.marginLeft = '100px';
      
  };
