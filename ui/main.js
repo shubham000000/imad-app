3027b2b0c89d1138b610cc3d9af6c980294eadb4
shubham000000 <borghare.sb@gmail.com>
2017-08-06 22:29:24 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,14 @@ var element = document.getElementById("main-text");
 element.innerHTML = 'New Value';
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
  img.onclick = function(){
+     var interval = setInterval(moveRight, 100);
     img.style.marginLeft = '100px';
      
  };
