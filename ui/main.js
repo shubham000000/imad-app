31d4184822164468a165fa059fc1fc81a9f5924e
shubham000000 <borghare.sb@gmail.com>
2017-08-08 07:11:00 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,66 +1,35 @@
-//counter code
-
 var button = document.getElementById('counter');
 
-button.onclick = function (){
- 
- 
- // Create a request object
- var request = new XMLHttpRequest();
- 
- //capture the response and store it in a variable
- request.onreadystatechange = function(){
-   if(request.readyState === XMLHttpRequest.DONE){
-       // Take some action
-       if(request.status === 200){
-           var counter = request.responseText;
-           var span = document.getElementById('count');
-           span.innerHTML = counter.toString();
-       }
-   }  
-   // Not done yet
- };
- 
-   // Make the request
-   var request = new XMLHttpRequest();
- 
- //capture the response and store it in a variable
- request.onreadystatechange = function(){
-   if(request.readyState === XMLHttpRequest.DONE){
-       // Take some action
-       if(request.status === 200){
-           var names = request.responseText;
-           names = JSON.parse(names);
-           var list = '';
-           for(var i = 0; i < names.length; i++){
-               list += '<li>' + names[i] + '</li>';
-           }
-           var ul = document.getElementById('namelist');
-           ul.innerHTML = list;
-       }
-   }  
-   // Not done yet
- };
- 
-   // Make the request
-   request.open('GET', 'http://borgharesb.imad.hasura-app.io/submit-name?name='+ name, true);
-   request.send(null);
+button.onclick = function(){
+     
+     var request = new XMLHttpRequest();
+     
+     request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.ONE)  {
+            
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+     };
+     
+     request.open('GET', 'http://borgharesb.imad.hasura-app.io/counter', true);
+     request.send(null);
 };
 
-
-// Submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
-  // Make a request to the server & send the name 
-  
-  // Capture a list of name & render it as a list
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  for(var i = 0; i < names.length; i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+submit.onclick =function(){
+    
+    var names= ['name1','name2','name3','name4'];
+    var list = ' ';
+    for (var i=0; i<names.lenght;i++){
+        list+= '<li>' + names[i] + '</li>';
+    }
+    
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
