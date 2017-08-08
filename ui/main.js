66ca5cd484707469a93ed4daaf60d7acccb416b4
shubham000000 <borghare.sb@gmail.com>
2017-08-08 05:04:06 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ button.onclick = function (){
    if(request.readyState === XMLHttpRequest.DONE){
        // Take some action
        if(request.status === 200){
-           var names = req.responseText;
+           var names = request.responseText;
            names = JSON.parse(names);
            var list = '';
            for(var i = 0; i < names.length; i++){
@@ -39,7 +39,7 @@ button.onclick = function (){
            ul.innerHTML = list;
        }
    }  
-  
+   // Not done yet
  };
  
    // Make the request
@@ -50,4 +50,18 @@ button.onclick = function (){
 
 
 // Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  // Make a request to the server & send the name 
+  
+  // Capture a list of name & render it as a list
+  var names = ['name1', 'name2', 'name3', 'name4'];
+  var list = '';
+  for(var i = 0; i < names.length; i++){
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
 };
