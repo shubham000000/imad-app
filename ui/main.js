2f2360c991a084b82466079877f0fed82d153c10
shubham000000 <borghare.sb@gmail.com>
2017-08-08 03:37:35 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,20 @@ button.onclick = function (){
    request.open('GET', 'http://borgharesb.imad.hasura-app.io/counter', true);
    request.send(null);
 };
+
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value();
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  // Make a request to the server & send the name 
+  
+  // Capture a list of name & render it as a list
+  var name = ['name1', 'name2', 'name3', 'name4'];
+  var list = ' ';
+  for(i = 0; i < name.length; i++){
+      list <- '<li' + names[i] + '<li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
