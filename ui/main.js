d5c095f91bfda0c5bc0f561887181acc9c219c08
shubham000000 <borghare.sb@gmail.com>
2017-08-08 03:03:41 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,27 @@
 //counter code
 
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function (){
  
  
- //make a request to the counter endpoint 
+ // Create a request object
+ var request = XMLHttpRequest();
  
  //capture the response and store it in a variable
+ request.onreadystatechange = function(){
+   if(request.readyState === XMLHttpRequest.DONE){
+       // Take some action
+       if(request.status === 200){
+           var counter = request.responseText;
+           var span = document.getElementById('count');
+           span.innerHTML = counter.toString();
+       }
+   }  
+   // Not done yet
+ };
  
- //render the variable in the correct span
- counter = counter + 1;
- var span = document.getElementById('count');
- span.innerHTML = counter.toString();
+   // Make the request
+   request.open('GET', 'http://borgharesb.imad.hasura-app.io/counter', true);
+   request.send(null);
 };
