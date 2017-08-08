bae64653eb63b6cbdeb4660b0ee060c224b04c50
shubham000000 <borghare.sb@gmail.com>
2017-08-08 09:00:17 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,11 +121,11 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names =[];
+var names = [];
 app.get('/submit-name/:name', function (req, res) {
-   var name = req.params.names;
+   var name = req.params.name;
    
-   names.push('name');
+   names.push(name);
    
    res.send(JSON.stringify(names));
 });
