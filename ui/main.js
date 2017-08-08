240a0fc1607f65b960cb5ad4603d60a0231f6b98
shubham000000 <borghare.sb@gmail.com>
2017-08-08 06:49:39 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,8 +33,8 @@ button.onclick = function (){
            names = JSON.parse(names);
            var list = '';
            for(var i = 0; i < names.length; i++){
-           list += '<li>' + names[i] + '</li>';
-  }
+               list += '<li>' + names[i] + '</li>';
+           }
            var ul = document.getElementById('namelist');
            ul.innerHTML = list;
        }
@@ -44,7 +44,6 @@ button.onclick = function (){
  
    // Make the request
    request.open('GET', 'http://borgharesb.imad.hasura-app.io/submit-name?name='+ name, true);
-   request.send(null);request.open('GET', 'http://borgharesb.imad.hasura-app.io/counter', true);
    request.send(null);
 };
 
