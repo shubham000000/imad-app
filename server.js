edc1e6a1c9d3b8f134000479ef781f33ff7a9811
shubham000000 <borghare.sb@gmail.com>
2017-08-14 03:00:46 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,10 +134,22 @@ app.get('/submit-name', function(req, res){ // URL:/ submit-name?name = xxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function(req,res){
-    var articleName= req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
-});
+app.get('/articles/:articleName',function(req,res){
+   
+    
+   pool.query("SELECT * FROM article WHERE title= " + req.params.articleName, function(err, result){
+       if(err){
+           result.status(500).send(err.toString());       
+           }else{
+               if(res.rows.length === 0){
+                   result.status(404).send('Articl not found');
+               }else{
+                   var articleData = result.rows[0];
+                   res.send(createTemplate(articleData));
+               }
+           }
+   });
+  });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
