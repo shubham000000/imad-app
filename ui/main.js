65439bebd458dad17d18711504e7807c642d8745
shubham000000 <borghare.sb@gmail.com>
2017-08-15 13:32:23 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,44 +1,30 @@
-var button = document.getElementById('counter');
 
-button.onclick = function () {
-    
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function(){
-      if(request.readyState === XMLHttpRequest.DONE)  {
-          if(request.status === 200){
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
-          }
-      }
-    };
-    
-     request.open ('GET', 'http://borgharesb.imad.hasura-app.io/counter', true);
-     request.send(null);
-};
 var submit = document.getElementById('submit_btn');
      submit.onclick = function () {
      var request = new XMLHttpRequest();
      request.onreadystatechange = function(){
       if(request.readyState === XMLHttpRequest.DONE)  {
           if(request.status === 200){
-           var names = request.responseText;
-           names = JSON.parse(names);
-           var list = '';
-           for(var i=0; i< names.length; i++){
-           list += '<li>' + names[i] + '</li>';
-     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+          
+          console.log('user logged in');
+          alert('Logged in successfully');
+    
+          }else if(request.status === 403){
+              alert('username/password is incorrect');
+          }else if(request.status === 500){
+              alert('something went wrong on the server');
           }
       }
     };
      
-     var nameInput = document.getElementById('name');
+     var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+     console.log(username);
+     console.log(password);
      var name = nameInput.value;
-     request.open('GET', 'http://borgharesb.imad.hasura-app.io/submit-name?name=' + name, true);
-     request.send(null);
+     request.open('POST', 'http://borgharesb.imad.hasura-app.io/login' + name, true);
+     request.setRequestHeader('Content-Type', 'application/json');
+     request.send(JSON.stringify({username: username, password: password}));
     
     
 };
